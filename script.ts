502d20f337c498e24a9cece5d6cfd38cbe500cb3
delima-justine <delimajustine24@gmail.com>
2025-09-15 00:07:01 +0800

    add TODO comments for member task assignments.

---
+++ script.ts
@@ -0,0 +1,15 @@
+// TODO(Ynion): Create an Enum with atleast 5 elements of any type.
+
+// TODO(Ynion): Create a Type with 5 properties.
+
+// TODO(Arroyo): Create an Object based on the Type created by Ynion.
+
+// TODO(Delima): Create an Object based on the Type created by Ynion.
+
+// TODO(Mejares): Create an Object based on the Type created by Ynion.
+
+// TODO(Ynion): Create an Object based on the Type created by Ynion.
+
+// TODO: (All Members): Call the print method you created in your object.
+
+// NOTE: Delete the comment once a task assigned is finished/completed.
