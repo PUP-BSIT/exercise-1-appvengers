2af0bcfb6a6a512d55ecdfb066e115b47e467a06
ynion-mabeamae <mabeamaeynion@gmail.com>
2025-09-15 08:44:23 +0800

    add enum, type and print calls

--- script.ts
+++ script.ts
@@ -1,6 +1,18 @@
-// TODO(Ynion): Create an Enum with atleast 5 elements of any type.
+enum KpopGroup {
+  BLACKPINK = "Blackpink",
+  NEWJEANS = "Newjeans",
+  EXO = "Exo",
+  BABYMONSTER = "Babymonster",
+  ENHYPEN = "Enhypen"
+}
 
-// TODO(Ynion): Create a Type with 5 properties.
+type Member = {
+  name: string;
+  age: number;
+  group: KpopGroup;
+  position: string;
+  print(): void;
+}
 
 // TODO(Arroyo): Create an Object based on the Type created by Ynion.
 
@@ -8,8 +20,38 @@
 
 // TODO(Mejares): Create an Object based on the Type created by Ynion.
 
-// TODO(Ynion): Create an Object based on the Type created by Ynion.
+
+const member4: Member = {
+  name: "Kim Minji",
+  age: 21,
+  group: KpopGroup.NEWJEANS,
+  position: "Rapper",
+  print() {
+    console.log(`
+      Name: ${this.name}
+      Age: ${this.age}
+      Group: ${this.group}
+      Position: ${this.position}
+    `);
+  },
+};
+
+const member5: Member = {
+  name: "Jake Sim",
+  age: 23,
+  group: KpopGroup.ENHYPEN,
+  position: "Vocalist",
+  print() {
+    console.log(`
+      Name: ${this.name}
+      Age: ${this.age}
+      Group: ${this.group}
+      Position: ${this.position}
+    `);
+  },
+};
 
 // TODO: (All Members): Call the print method you created in your object.
-
+member4.print();
+member5.print();
 // NOTE: Delete the comment once a task assigned is finished/completed.
